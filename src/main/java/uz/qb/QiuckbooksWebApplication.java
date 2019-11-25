a03d1cd7a5a5efae8924c5d9534afe3ad36cedc0
Alisher Kasimov <alisherkasimov1994@gmail.com>
2019-11-25 11:09:47 +0500

    Initial commit

---
+++ src/main/java/uz/qb/QiuckbooksWebApplication.java
@@ -0,0 +1,13 @@
+package uz.qb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiuckbooksWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QiuckbooksWebApplication.class, args);
+	}
+
+}
