12a1fc7c6379bc284995d4e772939320d9e2ebc7
Alisher Kasimov <alisherkasimov1994@gmail.com>
2019-11-30 16:28:26 +0500

    First commit

--- src/main/java/uz/qb/Application.java
+++ src/main/java/uz/qb/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QiuckbooksWebApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QiuckbooksWebApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
